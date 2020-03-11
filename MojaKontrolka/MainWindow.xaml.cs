54d456a72eb17775f2467c50da5525030be7a8e1
piotrekxyz <piotrekxyz@tlen.pl>
2020-03-11 14:25:46 +0100

    Add project files.

---
+++ MojaKontrolka/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MojaKontrolka
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Przycisk_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Helloł!");
+		}
+	}
+}
---
+++ MojaKontrolka/Przycisk.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MojaKontrolka
+{
+	public partial class Przycisk : UserControl
+	{
+		public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(String),
+			typeof(Przycisk), new FrameworkPropertyMetadata(string.Empty));
+
+		public string Text
+		{
+			get => GetValue(TextProperty).ToString();
+			set => SetValue(TextProperty, value);
+		}
+
+		public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource",
+			typeof(ImageSource), typeof(Przycisk), new FrameworkPropertyMetadata(null));
+
+		public ImageSource ImageSource
+		{
+			get { return GetValue(ImageSourceProperty) as ImageSource; }
+			set { SetValue(ImageSourceProperty, value); }
+		}
+
+		public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent("Click",
+			RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Przycisk));
+
+		public event RoutedEventHandler Click
+		{
+			add { AddHandler(ClickEvent, value); }
+			remove { RemoveHandler(ClickEvent, value); }
+		}
+
+		public Przycisk()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			RaiseEvent(new RoutedEventArgs(ClickEvent));
+		}
+	}
+}
